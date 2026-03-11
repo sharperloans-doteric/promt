8a306ed0ec857c2b8ca5719139e8bbf221d68fd7
sharperloans-doteric <sharperloans@gmail.com>
2026-03-11 08:51:59 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,20 @@
+const CACHE_NAME = 'secure-pay-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/logo.png'
+];
 
+// Install Service Worker
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+// Fetch Assets
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
+});
